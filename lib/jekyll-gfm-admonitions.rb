2935456bb8119c9e389e78d302dcf57d53aa39a3
manabu-nakamura <ma.nakamura@gmail.com>
2026-01-04 19:01:26 +0900

    Update

--- lib/jekyll-gfm-admonitions.rb
+++ lib/jekyll-gfm-admonitions.rb
@@ -36,8 +36,8 @@ module JekyllGFMAdmonitions
       original_content = content.dup
       content.gsub!(/<blockquote>\s*<p>\s*\[!(IMPORTANT|NOTE|WARNING|TIP|CAUTION)\](.*?)\n(.*?)\s*<\/p>\s*<\/blockquote>/m) do
         type  = ::Regexp.last_match(1).downcase
-#       title = (::Regexp.last_match(2).strip.empty? || ::Regexp.last_match(2).strip == '</p>') ? type.capitalize     : ::Regexp.last_match(2).strip
-        title = (::Regexp.last_match(2).strip.empty? || ::Regexp.last_match(2).strip == '</p>') ? ADMONITION_ja[type] : ::Regexp.last_match(2).strip
+        title = (::Regexp.last_match(2).strip.empty? || ::Regexp.last_match(2).strip == '</p>') ? type.capitalize     : ::Regexp.last_match(2).strip
+#       title = (::Regexp.last_match(2).strip.empty? || ::Regexp.last_match(2).strip == '</p>') ? ADMONITION_ja[type] : ::Regexp.last_match(2).strip
         text  = ::Regexp.last_match(3)
         icon  = Octicons::Octicon.new(ADMONITION_ICONS[type]).to_svg
         admonition_html(type, title, text, icon)
